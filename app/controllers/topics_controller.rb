031018c242498de598213dbe55b3fe8ce4a5ca76
khaiwyon <khaiwyon@gmail.com>
2016-08-25 18:24:00 +0800

    topic test

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -20,23 +20,25 @@ class TopicsController < ApplicationController
   end
 
   def edit
-    @topic = Topic.friendly.find(id: params[:id])
+    @topic = Topic.friendly.find(params[:id])
     authorize @topic
   end
 
   def update
-    @topic = Topic.friendly.find(id: params[:id])
+    @topic = Topic.friendly.find(params[:id])
     authorize @topic
 
     if @topic.update(topic_params)
-      flash.now[:success] = "You've successfully edited this topic."
+      redirect_to topics_path(@topic)
+      flash[:success] = "You've successfully edited this topic."
     else
-      flash.now[:danger] = @topic.errors.full_messages
+      redirect_to edit_topic_path(@topic)
+      flash[:danger] = @topic.errors.full_messages
     end
  end
 
  def destroy
-    @topic = Topic.friendly.find(id: params[:id])
+    @topic = Topic.friendly.find(params[:id])
     authorize @topic
     if @topic.destroy
       redirect_to topics_path
--- spec/controller/topic_controller_spec.rb
+++ spec/controller/topic_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe TopicsController, type: :controller do
   before(:all) do
     @admin = User.create(email: "hellokitty@gmail.com", password:"password", username: "hellokitty", role: 2)
     @user = User.create(email: "justanormaljoe@gmail.com", password:"password", username: "jojobeans", role: 0)
-    @topic= Topic.create(title: "I am not a whore", description: "But I like to do it do it do it do it")
+    @unauthorized_user = User.create(email: "nono@gmail.com", password: "password", username: "failure", role: 0)
+    @topic= Topic.create(title: "I am not a whore", description: "But I like to do it do it do it do it", user_id: "1")
   end
 
   describe "index topic" do
@@ -24,7 +25,6 @@ RSpec.describe TopicsController, type: :controller do
 
       expect(flash[:danger]).to eql("You need to login first!")
     end
-  end
 
     it "should create topic if logged in with admin" do
 
@@ -45,11 +45,65 @@ RSpec.describe TopicsController, type: :controller do
       post :create, xhr: true, params: epicparams, session: { id: @user.id }
 
       topic = Topic.find_by(title: "Gravity2")
-      binding.pry
       expect(Topic.count).to eql(2)
       expect(topic.title).to eql("Gravity2")
       expect(topic.description).to eql("John Mayer gets all the chicks wet2")
       expect(flash[:success]).to eql("You've created a new topic.")
+    end
+  end
+
+    describe "edit topic" do
+      it "should redirect if not logged in" do
+
+        params = { id: @topic.id }
+        get :edit, params: params
+
+        expect(subject).to redirect_to(root_path)
+        expect(flash[:danger]).to eql("You need to login first!")
+      end
+
+      it "should redirect if user unauthorized" do
+        params = { id: @topic.id, topic: { title: "Gravity", description: "John Mayer gets all the chicks wet"} }
+        patch :update, params: params, session: { id: @user.id }
+        expect(subject).to redirect_to(root_path)
+        expect(flash[:danger]).to eql("You're not authorized")
+      end
+
+      it "should render edit" do
+        params = { id: @admin.id }
+        get :edit, params: params, session: { id: @admin.id }
+
+        current_user = subject.send(:current_user)
+        expect(subject).to render_template(:edit)
+        expect(current_user).to be_present
+      end
+    end
+
+    describe "update topic" do
+      it "should update topic" do
+
+        epicparams = { id: @topic.id, topic: { title: "Gravity3", description: "John Mayer gets all the chicks wet3" } }
+        patch :update, params: epicparams, session: { id: @admin.id }
+
+        @topic.reload
+        expect(@topic.title).to eql("Gravity3")
+        expect(@topic.description).to eql("John Mayer gets all the chicks wet3")
+        expect(flash[:success]).to eql("You've successfully edited this topic.")
+      end
+
+      it "should redirect if not logged in" do
+        params = { id: @topic.id, topic: { title: "Gravity", description: "John Mayer gets all the chicks wet"} }
+        patch :update, params: params
+        expect(subject).to redirect_to(root_path)
+        expect(flash[:danger]).to eql("You need to login first!")
+      end
+
+      it "should redirect if user unauthorized" do
+        params = { id: @topic.id, topic: { title: "Gravity", description: "John Mayer gets all the chicks wet"} }
+        patch :update, params: params, session: { id: @user.id }
+        expect(subject).to redirect_to(root_path)
+        expect(flash[:danger]).to eql("You're not authorized")
+      end
     end
 
 
